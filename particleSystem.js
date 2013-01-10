f2cac5be5067214394ef883687e4fc96af5375dd
Turner Bohlen <turnerbohlen@gmail.com>
2013-01-10 01:08:30 -0500

    Adds collision resolution with backtracking

--- particleSystem.js
+++ particleSystem.js
@@ -184,7 +184,6 @@ ParticleSystem.prototype.doLogic = function(game) {
     for (key in this.particles) {
         var particle = this.particles[key];
         if (particle.isDead(this)) {
-            console.log("is dead");
             delete this.particles[key];
         }
     }
@@ -299,7 +298,7 @@ function ColorParticle(x, y, velX, velY, color) {
     Particle.call(this, x, y, velX, velY);
     this.state = [x, y, velX, velY, 0];
     this.color = color;
-    this.lives = 50;
+    this.lives = 4;
     this.radius = 4;
 }
 
@@ -320,22 +319,6 @@ ColorParticle.prototype.draw = function(game) {
     game.context.fillStyle = "rgb(" + Math.floor(color[0]).toString() + ", " + Math.floor(color[1]).toString() + ", " + Math.floor(color[2]).toString() + ")";
     drawCircle(this.state[0], this.state[1], this.radius, game.context);
     game.context.fill();
-};
-
-/*
- * Method: collidesWith
- * Returns true if this particle collides with the other circular particle
- * provided.
- *
- * Parameters:
- * that - the other circular particle to test for a collision with
- *
- * Member Of: ColorParticle
- */
-ColorParticle.prototype.collidesWith = function(that) {
-    var totRadius = that.radius + this.radius;
-    var totDistance = distance(this.state[0], this.state[1], that.state[0], that.state[1]);
-    return totRadius > totDistance;
 };
 
 /*
@@ -398,9 +381,35 @@ PlayerSystem.prototype.doLogic = function(game) {
     for (key in this.particles) {
         var particle = this.particles[key];
         if (particle.isDead(this)) {
+            // show a little flash
+            showFlash(particle.state[0], particle.state[1]);
             delete this.particles[key];
         }
+
+        // check for collisions
+        for (otherKey in this.others) {
+            var two = this.others[otherKey];
+            if (collides(particle, two)) {
+                resolveCollision(particle, two)
+
+                // subtract lives appropriately
+                var ourVel = distance(0, 0, particle.state[2], particle.state[3]);
+                var theirVel = distance(0, 0, this.others[otherKey].state[2], this.others[otherKey].state[3])
+                if (ourVel > theirVel + 10) {
+                    this.others[otherKey].lives--;
+                }
+                else if (ourVel < theirVel - 10){
+                    particle.lives--;
+                }
+                else {
+                    this.others[otherKey].lives--;
+                    particle.lives--;
+                }
+                break;
+            }
+        }
     }
+
     // step forward the system
     trapezoidalStep(this, 0.1);
     this.age++;
@@ -459,70 +468,101 @@ PlayerSystem.prototype.evalDeriv = function(state) {
     var key;
     var deriv = {};
     for (key in state) {
-        var done = false;
         var particle = state[key];
-        // check for collisions
-        for (otherKey in this.others) {
-            if (this.particles[key].collidesWith(this.others[otherKey])) {
-                //deriv[key] = [-1 * particle[2], -1 * particle[3], -2 * particle[2], -2 * particle[3], 1];
-                var ourVel = distance(0, 0, particle[2], particle[3]);
-                var theirVel = distance(0, 0, this.others[otherKey].state[2], this.others[otherKey].state[3])
-                if (ourVel > theirVel + 10) {
-                    this.others[otherKey].lives--;
-                }
-                else if (ourVel < theirVel - 10){
-                    this.particles[key].lives--;
-                }
-                else {
-                    this.others[otherKey].lives--;
-                    this.particles[key].lives--;
-                }
-                break;
-            }
+        var dist = distance(particle[0], particle[1], this.x, this.y);
+        var angleToCenter = Math.atan((this.y - particle[1])/(this.x - particle[0]));
+        if (this.x - particle[0] < 0){
+            angleToCenter = angleToCenter + Math.PI;
         }
 
-        // if their was no collision, move like normal
-        if (!done) {
-            var dist = distance(particle[0], particle[1], this.x, this.y);
-            var angleToCenter = Math.atan((this.y - particle[1])/(this.x - particle[0]));
-            if (this.x - particle[0] < 0){
-                angleToCenter = angleToCenter + Math.PI;
-            }
-
-            var angle = Math.atan(particle[3]/particle[2]);
-            if (angle != angle) {
-                angle = boundedRand(0, Math.PI * 2);
-            }
-            else if (particle[2] < 0){
-                angle = angle + Math.PI;
-            }
-
-            // randomize the angle
-            angle = angle + boundedRand(-1 * this.randomization, this.randomization);
-            
-            var sine = Math.sin(angle);
-            var cosine = Math.cos(angle);
-            var dir = new THREE.Vector2(cosine, sine);
-
-            dir.setLength(this.particleThruster);
-
-             //additional correcting force
-            if (dist > this.maxDist) {
-                var correction = new THREE.Vector2(Math.cos(angleToCenter), Math.sin(angleToCenter));
-                correction.setLength(Math.pow(dist / this.maxDist, this.correctionPower) * this.correctionForce);
-                dir.addSelf(correction)
-            }
-
-            // damp a tiny bit just to make sure we don't explode too quickly
-            var damping = new THREE.Vector2(particle[2], particle[3]);
-            var vel = damping.length()
-            damping.setLength(-1 * this.damping * vel * dist / (this.maxParticleVel * this.maxDist));
-
-            dir.addSelf(damping);
-
-            var newDeriv = [particle[2], particle[3], dir.x, dir.y, 1];
-            deriv[key] = newDeriv;
+        var angle = Math.atan(particle[3]/particle[2]);
+        if (angle != angle) {
+            angle = boundedRand(0, Math.PI * 2);
         }
+        else if (particle[2] < 0){
+            angle = angle + Math.PI;
+        }
+
+        // randomize the angle
+        angle = angle + boundedRand(-1 * this.randomization, this.randomization);
+        
+        var sine = Math.sin(angle);
+        var cosine = Math.cos(angle);
+        var dir = new THREE.Vector2(cosine, sine);
+
+        dir.setLength(this.particleThruster);
+
+         //additional correcting force
+        if (dist > this.maxDist) {
+            var correction = new THREE.Vector2(Math.cos(angleToCenter), Math.sin(angleToCenter));
+            correction.setLength(Math.pow(dist / this.maxDist, this.correctionPower) * this.correctionForce);
+            dir.addSelf(correction)
+        }
+
+        // damp a tiny bit just to make sure we don't explode too quickly
+        var damping = new THREE.Vector2(particle[2], particle[3]);
+        var vel = damping.length()
+        damping.setLength(-1 * this.damping * vel * dist / (this.maxParticleVel * this.maxDist));
+
+        dir.addSelf(damping);
+
+        var newDeriv = [particle[2], particle[3], dir.x, dir.y, 1];
+        deriv[key] = newDeriv;
     }
     return deriv;
 };
+
+/*
+ * Function: collides
+ * Checks to see if two particles collide. Particles must have round hitboxes.
+ *
+ * Parameters:
+ * one - one particle
+ * two - the other particle
+ */
+
+function collides(one, two) {
+    var totRadius = two.radius + one.radius;
+    var totDistance = distance(one.state[0], one.state[1], two.state[0], two.state[1]);
+    return totRadius > totDistance;
+};
+
+/*
+ * Function: resolveCollision
+ * Resolves a collision between the two particles. This does not handle the case
+ * that one and two do not in fact overlap. Passing two such objects will cause
+ * unknown behavior (probably nothing bad)
+ */
+function resolveCollision(one, two) {
+    var totRadius = one.radius + two.radius;
+    var centersVec = new THREE.Vector2(one.state[0] - two.state[0], one.state[1] - two.state[1]);
+    var totDistance = centersVec.length();
+    var totVel = new THREE.Vector2(one.state[2] + two.state[2], one.state[3] + two.state[3]);
+    var totVelMag = totVel.length();
+    // this is the normal pointing from one to two
+    var normal = centersVec.clone()
+    normal.normalize();
+    var rewindTime = Math.abs( (totRadius-totDistance) / totVelMag );
+    // recompute for one
+    var newPosOne = new THREE.Vector2(one.state[0] - rewindTime * one.state[2], one.state[1] - rewindTime * one.state[3]);
+    var newVelOne = new THREE.Vector2(one.state[2], one.state[3]);
+    var scaledNormalOne = normal.clone();
+    scaledNormalOne.setLength(-2 * newVelOne.dot(normal));
+    newVelOne.addSelf(scaledNormalOne)
+    var oneState = [newPosOne.x, newPosOne.y, newVelOne.x, newVelOne.y, 1];
+
+    // recompute for two
+    var newPosTwo = new THREE.Vector2(two.state[0] - rewindTime * two.state[2], one.state[1] - rewindTime * two.state[3]);
+    var newVelTwo = new THREE.Vector2(two.state[2], two.state[3]);
+    var scaledNormalTwo = normal.clone();
+    scaledNormalTwo.setLength(-2 * newVelTwo.dot(normal));
+    newVelTwo.addSelf(scaledNormalTwo)
+    var twoState = [newPosTwo.x, newPosTwo.y, newVelTwo.x, newVelTwo.y, 1];
+
+    // set the new states
+    one.state = oneState;
+    two.state = twoState;
+
+    // let the caller know the two objects collided
+    return true;
+}
